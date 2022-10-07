36b641afab2ae931200ce787f68382f2ab92a9e7
Sachitha Hirushan <shpsachitha@gmail.com>
2022-10-07 17:17:19 +0530

    wip

---
+++ app/src/main/java/com/example/rasachatbotapp/loginActivity.kt
@@ -0,0 +1,122 @@
+package com.example.rasachatbotapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.rasachatbotapp.R
+import com.example.rasachatbotapp.resources.Functions
+import org.json.JSONObject
+
+class loginActivity : AppCompatActivity() {
+
+    private var email : EditText? = null
+    private var password : EditText? = null
+    private var vistaPrincipal : View? = null
+    private var queue : RequestQueue? = null
+    private var pDialog : ProgressDialog? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        // CASTING FROM VIEWS TO KOTLIN OBJECTS
+        email = findViewById(R.id.loginEmail) as EditText
+        password = findViewById(R.id.loginPassword) as EditText
+        vistaPrincipal = findViewById(R.id.drawer_layout_login) as View
+
+        var btLogin : Button = findViewById(R.id.btLogin) as Button
+        var btnLinkToRegisterScreen : Button = findViewById(R.id.btnLinkToRegisterScreen) as Button
+
+        // We create the queue for the HTTP Requests of the Volley Library
+        queue = Volley.newRequestQueue(this)
+
+        // Progress dialog
+        pDialog = ProgressDialog(this)
+        pDialog?.setCancelable(false)
+
+        // Add the button register event when its clicked
+        btLogin.setOnClickListener {
+
+            // Hide the Keyboard
+            Functions.hideKeyboard(this.currentFocus, this)
+
+            // Collect the user input data
+            val e : String = email?.text.toString()
+            val p : String = password?.text.toString()
+
+            // Check if data its OK
+            if(!e.isEmpty() && !p.isEmpty())
+            {
+                try
+                {
+                    // Try to login
+                    attemptToLogin(e, p)
+                }
+                catch(excp : Exception)
+                {
+                    excp.printStackTrace()
+                }
+            }
+            else
+            {
+                // Show the error to the user
+                Functions.showSnackbar(vistaPrincipal!!, getString(R.string.errorFaltanDatos))
+            }
+        }
+
+        // Add the link to register activity
+        btnLinkToRegisterScreen.setOnClickListener {
+            var intent : Intent = Intent(this, registerActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    /**
+     * Method to try to login the user
+     * @param email : User email
+     * @param password : User password
+     */
+    private fun attemptToLogin(email : String, password : String)
+    {
+
+
+    }
+
+
+
+
+
+
+    private fun getDescriptionError(codError : String): String
+    {
+        var description : String = ""
+
+        when(codError) {
+            "Wrong_Credentials" -> description = getString(R.string.Wrong_Credentials)
+            "ErrorPostParamsMissing" -> description = getString(R.string.ErrorPostParamsMissing)
+        }
+
+        return description
+    }
+
+    /**
+     * Method to clean the user inputs
+     */
+    private fun cleanInputs()
+    {
+        email?.setText("")
+        password?.setText("")
+    }
+}
---
+++ app/src/main/java/com/example/rasachatbotapp/registerActivity.kt
@@ -0,0 +1,137 @@
+package com.example.rasachatbotapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.Volley
+import com.example.rasachatbotapp.R
+import com.example.rasachatbotapp.resources.Functions
+
+class registerActivity : AppCompatActivity() {
+
+    private var name : EditText? = null
+    private var email : EditText? = null
+    private var password : EditText? = null
+    private var queue : RequestQueue? = null
+    private var pDialog : ProgressDialog? = null
+    private var vistaPrincipal : View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+
+        // CASTING FROM VIEWS TO KOTLIN OBJECTS
+        name = findViewById(R.id.register_name) as EditText
+        email = findViewById(R.id.register_email) as EditText
+        password = findViewById(R.id.register_password) as EditText
+        vistaPrincipal = findViewById(R.id.drawer_layout_register) as View
+
+        var btnRegister : Button = findViewById(R.id.btRegistrarse) as Button
+        var btnLinkToLogin : Button = findViewById(R.id.btnLinkToLogIn) as Button
+
+        // We create the queue for the HTTP Requests of the Volley Library
+        queue = Volley.newRequestQueue(this)
+
+        // Progress Dialog
+        pDialog = ProgressDialog(this)
+        pDialog?.setCancelable(false)
+
+        // Add the button register event when its clicked
+        btnRegister.setOnClickListener {
+
+            // Hide the virtual keyboard
+            Functions.hideKeyboard(this.currentFocus, this)
+
+            // Collect the user data
+            val n : String = name?.text.toString()
+            val e : String = email?.text.toString()
+            val p : String = password?.text.toString()
+
+            // Check if data its OK
+            if(!n.isEmpty() && !e.isEmpty() && !p.isEmpty())
+            {
+                try {
+
+                    // Try to register
+                    attemptToRegister(n, e, p)
+                }
+                catch (excp : Exception)
+                {
+                    excp.printStackTrace()
+                }
+            }
+            else
+            {
+                // Show error message, we need all the user information filled
+//                Functions.showSnackbar(vistaPrincipal!!, getString(R.string.errorFaltanDatos))
+            }
+
+        }
+
+        // Add the link to login activity
+        btnLinkToLogin.setOnClickListener {
+            var intent : Intent = Intent(this, loginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    /**
+     * Method to register the user
+     * @param name : Username
+     * @param email : User email
+     * @param password : User password
+     */
+    private fun attemptToRegister(name : String, email : String, password : String)
+    {
+
+    }
+
+
+
+    private fun getDescriptionError(codError : String): String
+    {
+        var description : String = ""
+
+        when(codError) {
+            "UnknownErrorRegistration" -> description = getString(R.string.UnknownErrorRegistration)
+            "ErrorPostParamsMissing" -> description = getString(R.string.ErrorPostParamsMissing)
+            "UserAlreadyExist" -> description = getString(R.string.UserAlreadyExist)
+        }
+
+        return description
+    }
+
+    /**
+     * Method to clean the user inputs
+     */
+    private fun cleanInputs()
+    {
+        this.name?.setText("")
+        this.email?.setText("")
+        this.password?.setText("")
+    }
+
+    // Method executed when the Volley request fails
+    private fun requestError() : Response.ErrorListener
+    {
+        return Response.ErrorListener { error ->
+
+            // Show error message
+
+            // ! -- Remember if we get this error in Android com.android.volley.NoConnectionError: java.net.SocketException: socket failed: EACCES (Permission denied)
+            // its because we need to add an allowing permision to our app to access to Internet or local network in the manifest
+            // <uses-permission android:name="android.permission.INTERNET" />
+
+            Log.d("Volley", error.toString())
+            pDialog?.hide()
+//            Functions.showSnackbar(vistaPrincipal!!, getString(R.string.noHayConexionServidor))
+        }
+    }
+}
